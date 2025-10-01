8072cf476988388066c3b5e0a9ef00e6e7bf4c2b
Ndeyefa9928 <ndeye.mbow@groupe-esigelec.org>
2025-10-01 10:19:37 +0200

    refactor: remove unused pipeline configuration JSON files from data directory

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from command_builder.components.main_window import MainWindow
-from command_builder.services.pipeline_loader import load_pipelines
 from command_builder.services.yaml_pipeline_loader import load_yaml_pipelines
 
 
@@ -14,7 +13,6 @@ def setup_application():
 
 if __name__ == "__main__":
     app = setup_application()
-    # pipelines = load_pipelines()
     pipelines = load_yaml_pipelines()
     main_window = MainWindow()
     main_window.set_pipelines(pipelines)
