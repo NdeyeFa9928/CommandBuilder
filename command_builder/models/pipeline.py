f670589bdd871fcf0d53066c9a8eafde6a92682c
Ndeyefa9928 <ndeye.mbow@groupe-esigelec.org>
2025-10-01 14:43:17 +0200

    refactor (services) : remove YAML pipeline loader and related models in favor of new implementation

--- command_builder/models/pipeline.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List
-from command_builder.models.task import Task
-
-
-class Pipeline(BaseModel):
-    name: str
-    description: str
-    tasks: List[Task]
