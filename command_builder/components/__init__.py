861512db96c533b9acf3208f547eb5a2469f4312
Ndeyefa9928 <ndeye.mbow@groupe-esigelec.org>
2025-10-02 14:52:57 +0200

    feat( (components) : add TaskComponent with UI, styling and click handling

--- command_builder/components/__init__.py
+++ command_builder/components/__init__.py
@@ -7,3 +7,16 @@ from .main_window import MainWindow
 from .task_list import TaskList
 from .command_form import CommandForm
 from .console_output import ConsoleOutput
+from .task_component import TaskComponent
+from .argument_component import ArgumentComponent
+from .command_component import CommandComponent
+
+__all__ = [
+    "MainWindow",
+    "TaskList",
+    "CommandForm",
+    "ConsoleOutput",
+    "TaskComponent",
+    "ArgumentComponent",
+    "CommandComponent",
+]
