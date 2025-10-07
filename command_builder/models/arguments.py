6f71e757bd0bc4d9686c97bda5a1c827369e6120
Ndeyefa9928 <ndeye.mbow@groupe-esigelec.org>
2025-10-07 09:35:22 +0200

    feat (models) : add type, required, default and validation fields to Argument model

--- command_builder/models/arguments.py
+++ command_builder/models/arguments.py
@@ -1,8 +1,12 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Any
 
 
 class Argument(BaseModel):
     code: str
     name: str
     description: Optional[str] = ""
+    type: Optional[str] = "string"
+    required: Optional[int] = 0
+    default: Optional[str] = ""
+    validation: Optional[dict[str, Any]] = None
