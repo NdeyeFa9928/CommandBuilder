886b1d32222bb058c0747fc7510efec7ef0d7442
Ndeyefa9928 <ndeye.mbow@groupe-esigelec.org>
2025-11-25 15:18:56 +0100

    feat (build): add data directory copying to distribution and bump version to 0.2.0

--- command_builder/__init__.py
+++ command_builder/__init__.py
@@ -2,7 +2,7 @@
 CommandBuilder - GUI Assistant for Windows CLI Commands
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "CommandBuilder Team"
 __description__ = (
     "GUI Assistant for composing Windows CLI commands without syntax errors"
