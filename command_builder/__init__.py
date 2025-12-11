1a3bf2af4e70487674ee3d054e30d9f4445050cb
Ndeyefa9928 <ndeye.mbow@groupe-esigelec.org>
2025-12-11 10:13:13 +0100

    chore (version): bump version to 0.3.3

--- command_builder/__init__.py
+++ command_builder/__init__.py
@@ -2,7 +2,7 @@
 CommandBuilder - GUI Assistant for Windows CLI Commands
 """
 
-__version__ = "0.2.3"
+__version__ = "0.3.3"
 __author__ = "CommandBuilder Team"
 __description__ = (
     "GUI Assistant for composing Windows CLI commands without syntax errors"
